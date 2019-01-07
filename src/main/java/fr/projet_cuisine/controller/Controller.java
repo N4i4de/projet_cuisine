73427060c1cbe32f6c254ffee468c446d4dfd920
n4i4de <sandrine.lementec@gmail.com>
2019-01-07 11:54:07 +0100

    reprise projet : Gestion Ingredienf

--- src/main/java/fr/projet_cuisine/controller/Controller.java
+++ src/main/java/fr/projet_cuisine/controller/Controller.java
@@ -1,11 +1,15 @@
 package fr.projet_cuisine.controller;
 
 import fr.projet_cuisine.model.Aliment;
+import fr.projet_cuisine.model.Ingredient;
 import fr.projet_cuisine.repository.AlimentRepository;
 import fr.projet_cuisine.service.GestionAliment;
+import fr.projet_cuisine.service.GestionIngredient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class Controller {
@@ -15,12 +19,17 @@ public class Controller {
 
 	@Autowired
 	private GestionAliment gestionAliment;
+
+	@Autowired
+	private GestionIngredient gestionIngredient;
 	
-	@GetMapping("/all")
+	@GetMapping("/allAliment")
 	public Iterable<Aliment> listAll() {
 		return gestionAliment.getAllAliments();
 	}
 
+	@GetMapping("/allIngredient")
+	public List<Ingredient> listAllIngredient() {return gestionIngredient.getAllIngredients(); }
 
 	@GetMapping
 	public String test() {
